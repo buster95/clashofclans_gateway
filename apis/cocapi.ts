45f614ea0aa3fc19ac20a60185f2da885b9976fc
Walter Corrales <corraleswalter@live.com>
2020-09-28 13:46:49 -0600

    trying to solve proxy connection

--- apis/cocapi.ts
+++ apis/cocapi.ts
@@ -1,38 +1,39 @@
 import axios from 'axios';
 import express, { Router } from 'express';
+import url from 'url';
 
 let cocapi = Router();
 
-import url from 'url';
 const proxyUrl = url.parse(process.env.IPB_HTTP || 'http://zxtrtr:t+helg@c@66-154-123-139.ip.heroku.ipb.cloud:9080');
-const requestUrl = url.parse('https://api.clashofclans.com/v1');
-const proxyConfig = { host: proxyUrl.hostname || '', port: Number(proxyUrl.port) };
-console.log(proxyConfig, `Basic ${Buffer.from(proxyUrl.auth || '').toString('base64')}`);
-try {
-    var http = axios.create({
-        proxy: {
-            ...proxyConfig,
-            // auth: {
-            //     username: 'zxtrtr',
-            //     password: 't+helg@c'
-            // }
-        },
-        headers: {
-            Host: requestUrl.host,
-            'Proxy-Authorization': `Basic ${Buffer.from(proxyUrl.auth || '').toString('base64')}`,
-        },
-        baseURL: 'https://api.clashofclans.com/v1',
-        timeout: 10000,
-        // httpAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.74 Safari/537.36 Edg/79.0.309.43',
-    });
-    http.interceptors.response.use((response) => {
-        return response;
-    }, (error) => {
-        return Promise.reject(error);
-    });
-} catch (error) {
-    console.error(error);
-}
+const requestUrl = url.parse('https://api.clashofclans.com');
+const proxyConfig = {
+    host: proxyUrl.hostname || '',
+    port: Number(proxyUrl.port),
+    auth: {
+        username: 'zxtrtr',
+        password: 't+helg@c'
+    }
+};
+console.log(proxyConfig);
+console.log(proxyUrl.auth);
+console.log(`Basic ${Buffer.from(proxyUrl.auth || '').toString('base64')}`);
+console.log(requestUrl.host);
+
+var http = axios.create({
+    proxy: proxyConfig,
+    headers: {
+        // Host: requestUrl.host,
+        'Proxy-Authorization': `Basic ${Buffer.from(proxyUrl.auth || '').toString('base64')}`,
+    },
+    baseURL: 'https://api.clashofclans.com/v1',
+    timeout: 10000,
+    // httpAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.74 Safari/537.36 Edg/79.0.309.43',
+});
+http.interceptors.response.use((response) => {
+    return response;
+}, (error) => {
+    return Promise.reject(error);
+});
 
 cocapi.use(express.json());
 cocapi.use(express.urlencoded({ extended: true }));
@@ -52,20 +53,22 @@ cocapi.use('/cocapi/*', async (req, res) => {
 
     try {
         let result = await http.get(url, {
-            headers: {
-                authorization
-            }
+            headers: { authorization }
         });
         console.log('ok');
         res.json(result.data);
-        // .then(result => {
+        // http.get(url, {
+        //     headers: {
+        //         authorization
+        //     }
+        // }).then(result => {
         // console.log('ok');
         // res.json(result.data);
         // }).catch(error => {
         //     res.status(error.response?.status).json(error.response?.data || { message: error.response?.statusText });
         // });
     } catch (error) {
-        console.log(error);
+        // console.log(error);
         res.status(error.response?.status).json(error.response?.data || { message: error.response?.statusText });
     }
 });
