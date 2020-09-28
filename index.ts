3088038bb33f46df07f993810ed0378ae4968665
Walter Corrales <corraleswalter@live.com>
2020-09-27 18:10:34 -0600

    adding cors policies

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import { cocapi } from './apis';
 
 let app = express();
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
