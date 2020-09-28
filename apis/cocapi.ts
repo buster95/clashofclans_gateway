a290a24b6223160d04eabad6f54e9e7403a7a998
Walter Corrales <corraleswalter@live.com>
2020-09-28 09:54:55 -0600

    proxy configuration IPBurguer on heroku

--- apis/cocapi.ts
+++ apis/cocapi.ts
@@ -3,7 +3,17 @@ import express, { Router } from 'express';
 
 let cocapi = Router();
 
+import url from 'url';
+const proxyUrl = url.parse(process.env.IPB_HTTP || '');
+const requestUrl = url.parse('https://api.clashofclans.com/v1');
+const proxyConfig = process.env.IPB_HTTP ? { host: proxyUrl.hostname || '', port: Number(proxyUrl.port) } : false;
+
 var http = axios.create({
+    proxy: proxyConfig,
+    headers: {
+        Host: requestUrl.host,
+        'Proxy-Authorization': `Basic ${Buffer.from(proxyUrl.auth || '').toString('base64')}`,
+    },
     baseURL: 'https://api.clashofclans.com/v1',
     timeout: 10000,
     httpAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.74 Safari/537.36 Edg/79.0.309.43',
